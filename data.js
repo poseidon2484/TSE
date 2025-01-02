d26a51c684cfba6319be1ade69ab49d2e06f9c29
sidshetty01 <sidshettyedu@gmail.com>
2025-01-02 18:56:12 +0530

    added toggle menu,to top button

--- data.js
+++ data.js
@@ -1,6 +1,39 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwW-LJNg_n_pPfyo5RiMOYDgQgjn8iiE-5CmVqqrWiFmxDLWsdIJZWnjqHsSORDpc3S/exec';
 
 const form = document.forms['contact-form'];
+function toggleMenu() {
+  const navLinks = document.getElementById('navLinks');
+  if (navLinks.style.right === '0px') {
+      navLinks.style.right = '-200px';
+  } else {
+      navLinks.style.right = '0px';
+  }
+}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+const scrollToTopButton = document.getElementById('scrollToTop');
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+};
+
+scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
 
 form.addEventListener('submit', e => {
   e.preventDefault();
